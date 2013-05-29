432c8945440ad60ad07d55f09f35720027f113a1
btanders@asu.edu <btanders@asu.edu@2e2b0403-20f3-19e0-d07a-92f660574032>
2013-05-29 22:57:47 +0000

    updated

--- ShellShort/test/shellshort/ShellShortTest.java
+++ ShellShort/test/shellshort/ShellShortTest.java
@@ -21,7 +21,41 @@ import static org.junit.Assert.*;
  */
 public class ShellShortTest {
     
+    int[][] testCases;
+    int[][] expResults;
+    
     public ShellShortTest() {
+        List<Integer> test = generateRandomArray(1024, 100);
+        int[] rand = toIntArray(test);     
+        Collections.sort(test);
+        int[] randSorted = toIntArray(test);
+        
+        test = generateRandomArray(100, Integer.MAX_VALUE);
+        int[] randMax = toIntArray(test);     
+        Collections.sort(test);
+        int[] randSortedMax = toIntArray(test);
+        
+        testCases = new int[][] {
+            null,
+            { },
+            {23},
+            {1,2,3,4,5},
+            {5,4,3,2,1},
+            {2,2,4,5,1,3,5},
+            rand,
+            randMax
+        };
+        
+        expResults = new int[][] {
+            null,
+            { },
+            {23},
+            {1,2,3,4,5},
+            {1,2,3,4,5},
+            {1,2,2,3,4,5,5},
+            randSorted,
+            randSortedMax
+        };
     }
     
     @BeforeClass
@@ -47,91 +81,22 @@ public class ShellShortTest {
     public void testShellSort() {
         System.out.println("ShellSort");
         
-        System.out.println("Test Case 1: Null array");
-        int[] array1 =      null;
-        int[] expResult1 =  null;
-        System.out.print("Tested array:\t");
-        ShellShort.printArray(array1);
-        System.out.print("Expected array:\t");
-        ShellShort.printArray(expResult1);
-        int[] result = ShellShort.ShellSort(array1);
-        System.out.print("Result array:\t");
-        ShellShort.printArray(result);
-        assertArrayEquals(expResult1, result);
-        
-        System.out.println("\nTest Case 2: Empty array");
-        int[] array2 =      {};
-        int[] expResult2 =  {};
-        System.out.print("Tested array:\t");
-        ShellShort.printArray(array2);
-        System.out.print("Expected array:\t");
-        ShellShort.printArray(expResult2);
-        result = ShellShort.ShellSort(array2);
-        System.out.print("Result array:\t");
-        ShellShort.printArray(result);
-        assertArrayEquals(expResult2, result);
-        
-        System.out.println("\nTest Case 3: Single element array");
-        int[] array3 =      {23};
-        int[] expResult3 =  {23};
-        System.out.print("Tested array:\t");
-        ShellShort.printArray(array3);
-        System.out.print("Expected array:\t");
-        ShellShort.printArray(expResult3);
-        result = ShellShort.ShellSort(array3);
-        System.out.print("Result array:\t");
-        ShellShort.printArray(result);
-        assertArrayEquals(expResult3, result);
-        
-        System.out.println("\nTest Case 4: Sorted array");
-        int[] array4 =      {1,2,3,4,5};
-        int[] expResult4 =  {1,2,3,4,5};
-        System.out.print("Tested array:\t");
-        ShellShort.printArray(array4);
-        System.out.print("Expected array:\t");
-        ShellShort.printArray(expResult4);
-        result = ShellShort.ShellSort(array4);
-        System.out.print("Result array:\t");
-        ShellShort.printArray(result);
-        assertArrayEquals(expResult4, result);
-        
-        System.out.println("\nTest Case 5: Reverse-sorted array");
-        int[] array5 =      {5,4,3,2,1};
-        int[] expResult5 =  {1,2,3,4,5};
-        System.out.print("Tested array:\t");
-        ShellShort.printArray(array5);
-        System.out.print("Expected array:\t");
-        ShellShort.printArray(expResult5);
-        result = ShellShort.ShellSort(array5);
-        System.out.print("Result array:\t");
-        ShellShort.printArray(result);
-        assertArrayEquals(expResult5, result);
-        
-        System.out.println("\nTest Case 6: Array with duplicate elements");
-        int[] array6 =      {2,2,4,5,1,3,5};
-        int[] expResult6 =  {1,2,2,3,4,5,5};
-        System.out.print("Tested array:\t");
-        ShellShort.printArray(array6);
-        System.out.print("Expected array:\t");
-        ShellShort.printArray(expResult6);
-        result = ShellShort.ShellSort(array6);
-        System.out.print("Result array:\t");
-        ShellShort.printArray(result);
-        assertArrayEquals(expResult6, result);
-        
-        System.out.println("\nTest Case 7: Random array");
-        List<Integer> test = generateRandomArray(100, 4096);
-        int[] array7 =      toIntArray(test);
-        Collections.sort(test);
-        int[] expResult7 =  toIntArray(test);
-        System.out.print("Tested array:\t");
-        ShellShort.printArray(array7);
-        System.out.print("Expected array:\t");
-        ShellShort.printArray(expResult7);
-        result = ShellShort.ShellSort(array7);
-        System.out.print("Result array:\t");
-        ShellShort.printArray(result);
-        assertArrayEquals(expResult7, result);
+        for (int i = 0; i < testCases.length; ++i) {
+            System.out.println("\nTest Case " + i);
+            
+            System.out.print("Tested array:\t");
+            ShellShort.printArray(testCases[i]);
+            
+            System.out.print("Expected array:\t");
+            ShellShort.printArray(expResults[i]);
+            
+            int[] result = ShellShort.ShellSort(testCases[i]);
+            
+            System.out.print("Result array:\t");
+            ShellShort.printArray(result);
+            
+            assertArrayEquals(expResults[i], result);
+        }
     }
     
     public static ArrayList<Integer> generateRandomArray(int size, int maxInt) {
