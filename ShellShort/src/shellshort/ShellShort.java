76f01d6791191c64125d1282674bae4916f8b8d4
btanders@asu.edu <btanders@asu.edu@2e2b0403-20f3-19e0-d07a-92f660574032>
2013-05-29 23:41:45 +0000

    updated

--- ShellShort/src/shellshort/ShellShort.java
+++ ShellShort/src/shellshort/ShellShort.java
@@ -8,13 +8,18 @@ package shellshort;
  *
  * @author Brandon
  */
-public class ShellShort {
-
+public class ShellShort
+{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        int array[] = {5,4,3,2,1};
+    public static void main(String[] args)
+    {
+        // Simple test case in main...
+        int array[] =
+        {
+            5, 4, 3, 2, 1
+        };
 
         printArray(array);
 
@@ -23,52 +28,88 @@ public class ShellShort {
         printArray(array);
     }
 
-    public static int[] ShellSort(int[] array) {
-
-        if (null != array) {
+    /**
+     * ShellSort - Implemented using Knuth's linear recurrence relationship of h(s + 1) = 3 * h(s) + 1 where h(1) = 1, s >= 1     * 
+     * @param array - this gets sorted in place but is returned as well
+     * @return int[] - the sorted array
+     */
+    public static int[] ShellSort(int[] array)
+    {
+        // Check to see if the array passed is null, if so, we will do no work and just return null
+        if (null != array)
+        {
+            // temporary int for swapping
             int temp;
 
+            // iterators for the array
             int i = 0;
-            int k = 0;
+            int j = 0;
 
-            int sequence = 1;
+            // the initial gap of h(1) = 1
+            int gap = 1;
 
-            while (sequence <= array.length / 3) {
-                sequence = sequence * 3 + 1;
+            // calculate the first gap based on the length of the array to sort
+            while (gap <= array.length / 3)
+            {
+                gap = gap * 3 + 1;
             }
 
-            while (sequence > 0) {
-                for (k = sequence; k < array.length; ++k) {
-                    temp = array[k];
-                    i = k;
+            // sorting
+            while (gap > 0)
+            {
+                // check at the gap
+                for (j = gap; j < array.length; ++j)
+                {
+                    // get the value at the gap
+                    temp = array[j];
+                    i = j;
 
-                    while (i > sequence - 1 && array[i - sequence] >= temp) {
-                        array[i] = array[i - sequence];
-                        i -= sequence;
+                    // do swaps between the value at the gap and those at (i - gap) if not in sorted order
+                    while ( ( i > ( gap - 1 ) ) && 
+                            (array[i - gap] >= temp) )
+                    {
+                        array[i] = array[i - gap];
+                        i -= gap;
                     }
 
                     array[i] = temp;
                 }
 
-                sequence = (sequence - 1) / 3;
+                // calculate the next gap
+                gap = (gap - 1) / 3;
             }
         }
 
+        // return the results
         return array;
     }
 
-    public static void printArray(int[] array) {
-        if (null == array) {
+    /**
+     * printArray - just provides a nicely printed output of the array for visual verification
+     * @param array 
+     */
+    public static void printArray(int[] array)
+    {
+        if (null == array)
+        {
             System.out.println("NULL");
-        } else {
+        }
+        else
+        {
             System.out.print("[ ");
-            for (int i = 0; i < array.length; ++i) {
-                if (i == array.length - 1) {
+            
+            for (int i = 0; i < array.length; ++i)
+            {
+                if (i == array.length - 1)
+                {
                     System.out.print(array[i]);
-                } else {
+                }
+                else
+                {
                     System.out.print(array[i] + " | ");
                 }
             }
+            
             System.out.println(" ]");
         }
     }
